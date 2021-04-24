ef10c0245a55ba217673e7eb9d45b1f2eca50266
MaheshB77 <mbansode7696@gmail.com>
2021-04-24 22:41:00 +0530

    Added user model

--- src/main/java/com/kafka/demo/springbootkafka/services/KafkaService.java
+++ src/main/java/com/kafka/demo/springbootkafka/services/KafkaService.java
@@ -1,5 +1,8 @@
 package com.kafka.demo.springbootkafka.services;
 
+import com.kafka.demo.springbootkafka.models.User;
+
 public interface KafkaService {
     public String produceMessage(String message);
+    public String produceUser(User user);
 }
--- src/main/java/com/kafka/demo/springbootkafka/services/KafkaServiceImpl.java
+++ src/main/java/com/kafka/demo/springbootkafka/services/KafkaServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kafka.demo.springbootkafka.services;
 
+import com.kafka.demo.springbootkafka.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,15 @@ public class KafkaServiceImpl implements KafkaService{
         try {
             this.kafkaTemplate.send(kafkaTopic, message);
             return message + " Produced successfully";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public String produceUser(User user) {
+        try {
+            return "";
         } catch (Exception e) {
             return e.getMessage();
         }
