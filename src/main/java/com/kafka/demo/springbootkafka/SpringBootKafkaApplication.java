62d431b056fe95b0302047109a9b6168ad2bac26
MaheshB77 <mbansode7696@gmail.com>
2021-04-24 15:45:09 +0530

    Initial commit

--- src/main/java/com/kafka/demo/springbootkafka/SpringBootKafkaApplication.java
+++ src/main/java/com/kafka/demo/springbootkafka/SpringBootKafkaApplication.java
@@ -1,5 +1,6 @@
 package com.kafka.demo.springbootkafka;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class SpringBootKafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKafkaApplication.class, args);
+		System.out.println("spring-boot-kafka started on port 8081");
 	}
 
 }
